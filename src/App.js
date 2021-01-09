54777b27f4bcf812447a897935b3a49926725064
Robbiexd <robert.hak@pslib.cz>
2021-01-09 02:32:53 +0100

    mensi test vim editoru

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Order from './components/order';
 
 function App() {
   return (
     <div className="App">
-      
+        <Order></Order>
     </div>
   );
 }
--- src/components/order.jsx
+++ src/components/order.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Pizza(){
+function Order(){
     return(
         <>
+            <p>Order</p>
         </>
     )
 }
 
-export default Pizza;
+export default Order;
